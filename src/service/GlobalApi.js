7ea10c61c441b43f4a7f9fe849a2e4151245031b
kaushal1717 <kpadaliya1717@gmail.com>
2024-09-28 20:03:05 +0530

    changed: baseURL

--- src/service/GlobalApi.js
+++ src/service/GlobalApi.js
@@ -2,7 +2,7 @@ import { default as axios } from "axios";
 
 const API_KEY = import.meta.env.VITE_STRAPI_API;
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api/",
+  baseURL: "https://careerai-cms.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${API_KEY}`,
